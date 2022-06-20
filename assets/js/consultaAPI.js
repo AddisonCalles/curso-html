55ed1fcc00dab220cd58d1a92477b36277ba5b46
Addison Calles <addisoncalles@gmail,com>
2022-06-19 19:04:01 -0500

    Request http con fetch

---
+++ assets/js/consultaAPI.js
@@ -0,0 +1,61 @@
+
+function addRegistro(tbody, id, nombre, edad, nacionalidad){
+    //Crear fila
+    const fila = document.createElement('tr'); // <tr></tr>
+
+    //Crear columnas
+    const colId = document.createElement('th');
+    const colName = document.createElement('td');
+    const colEdad = document.createElement('td');
+    const colNacionalidad = document.createElement('td');
+
+    //Modificar atributos a columnas
+    colId.setAttribute('scope','row');
+
+    //Modificar el contenido a columnas
+    colId.innerText = id;
+    colName.innerText = nombre;
+    colEdad.innerText = edad;
+    colNacionalidad.innerText = nacionalidad;
+
+    //Agregar las columnas a la fila
+    fila.append(colId);
+    fila.append(colName);
+    fila.append(colEdad);
+    fila.append(colNacionalidad);
+
+    //Agregar la fila al tbody
+    tbody.append(fila);
+}
+
+/**
+ * DOCUMENTACION DE APIS:
+ * URLS
+ * PARAMETROS
+ * METODOS
+ * RESPUESTAS
+ * 
+ */
+async function getUsers(nombre){
+   let url = `http://localhost:3000/users`;
+   if(nombre) url = url + `/${nombre}`;
+   const response = await fetch(url);
+   const users = response.json();
+   return users;
+} 
+
+async function buscar(){
+    const inputName = document.querySelector('#inputname');
+    const tbody = document.querySelector('.table-users tbody');
+    //const users = document.querySelectorAll('.table-users tbody tr');
+    tbody.innerHTML = "";
+    const users = await getUsers(inputName.value);
+    //tbody, id, nombre, edad, nacionalidad
+    for (const user of users) {
+        addRegistro(tbody, user.id, user.nombre, user.edad, user.nacionalidad);
+        console.log(user);
+     
+    }
+
+    
+}
